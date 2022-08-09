b263e72b989d9ef1c20f94d505b04591976ac202
ad3rinto <aad3rinto@gmail.com>
2022-08-09 21:59:21 +0100

    adding starting files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+    const URL = "https://api.openweathermap.org/data/2.5/weather?appid=9f8040ea808f9433a3acc74797ea8910&q=stockport&units=metric"
+
+    https.get(URL, function(response){
+        console.log(response.statusCode)
+    })
+    res.sendFile(__dirname+"/index.html")
+})
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000")
+})

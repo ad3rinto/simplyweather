5832f3bbe7397a8f481eb1de128fe8db30ff4c54
ad3rinto <aad3rinto@gmail.com>
2022-08-10 17:24:19 +0100

    pulled icon from API

--- app.js
+++ app.js
@@ -11,9 +11,20 @@ app.get("/", function(req, res){
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            console.log(weatherData.weather[0].description)
+            const weatherDisc = weatherData.weather[0].description
             const temp = weatherData.main.temp
-            res.send(`<h1>The current temperature in your area is ${temp} degrees Celcius</h1>`)
+            const icon = weatherData.weather[0].icon 
+            console.log("Icon ID is "+ icon)
+            const imgLink = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+
+
+
+            res.send(`<div>
+            <h3>And the weather is currently ${weatherDisc}</h3>
+            <h1>The current temperature in your area is ${temp} degrees Celcius</h1>
+            <img src = ${imgLink}>
+            `)
         })
     })
 })
